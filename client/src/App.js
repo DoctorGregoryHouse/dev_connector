97fc995f06b4e90a9584fba4eb7bc3c883f607c0
DoctorGregoryHouse(W) <dev.rkleinert@gmail.com>
2020-03-27 14:51:39 +0100

    Add AddExperience, AddEducation

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+//profile-forms
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
-import PrivateRoute from './components/routing/PrivateRoute';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import './App.css';
 //Redux
 import { loadUser } from './actions/auth';
@@ -56,6 +59,16 @@ const App = () => {
                                 exact
                                 path="/edit-profile"
                                 component={EditProfile}
+                            />
+                            <PrivateRoute
+                                exact
+                                path="/add-education"
+                                component={AddEducation}
+                            />
+                            <PrivateRoute
+                                exact
+                                path="/add-experience"
+                                component={AddExperience}
                             />
                         </Switch>
                     </section>
